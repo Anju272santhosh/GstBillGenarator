50bf612e269f2a05a56cb5cc51648b4f4382376b
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-05 21:32:01 +0530

    feat(App): Root Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import CategoryForm from './CategoryForm';
+import ProductForm from './ProductForm';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [categories, setCategories] = useState(() => {
+    const savedCategories = localStorage.getItem('categories');
+    return savedCategories ? JSON.parse(savedCategories) : [];
+  });
+
+  const [products, setProducts] = useState(() => {
+    const savedProducts = localStorage.getItem('products');
+    return savedProducts ? JSON.parse(savedProducts) : [];
+  });
+
+  // Add a new category to the state and local storage
+  const addCategory = (newCategory) => {
+    setCategories([...categories, newCategory]);
+  };
+
+  // Add a new product to the state and local storage
+  const addProduct = (newProduct) => {
+    setProducts([...products, newProduct]);
+  };
+
+  // Update local storage whenever categories or products change
+  useEffect(() => {
+    localStorage.setItem('categories', JSON.stringify(categories));
+  }, [categories]);
+
+  useEffect(() => {
+    localStorage.setItem('products', JSON.stringify(products));
+  }, [products]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>GST System</h1>
 
-export default App
+      {/* Category Form to add a new category */}
+      <h2>Add Category</h2>
+      <CategoryForm addCategory={addCategory} />
+
+      {/* Display Categories */}
+      <h2>Categories</h2>
+      <CategoryList categories={categories} />
+
+      {/* Product Form to add a new product */}
+      <h2>Add Product</h2>
+      <ProductForm categories={categories} addProduct={addProduct} />
+
+      {/* Display Products */}
+      <h2>Products</h2>
+      <ProductList products={products} />
+
+    </div>
+  );
+};
+
+export default App;
