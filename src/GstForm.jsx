c14c45fa3586c31d09046cdbe9e7f55a14409097
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-05 21:39:39 +0530

    feat(Gst): Gst Calculations

---
+++ src/GstForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const GSTForm = ({ setGST }) => {
+  const [category, setCategory] = useState('');
+  const [gstRate, setGSTRate] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setGST({ category, rate: gstRate });
+    setCategory('');
+    setGSTRate('');
+  };
+
+  return (
+    <div>
+      <h2>Set GST Rates</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} placeholder="Product Category" />
+        <input type="number" value={gstRate} onChange={(e) => setGSTRate(e.target.value)} placeholder="GST Rate" />
+        <button type="submit">Set GST Rate</button>
+      </form>
+    </div>
+  );
+};
+
+export default GSTForm;
