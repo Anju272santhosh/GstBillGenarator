9fc2a906b3edd36fe58410c736688e249e7c68f2
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-06 01:32:21 +0530

    fix(CategoryList): delete and update gst

--- src/CategoryList.jsx
+++ src/CategoryList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const CategoryList = ({ categories }) => {
+const CategoryList = ({ categories, handleDelete, handleGSTUpdate }) => {
   return (
     <table>
       <thead>
         <tr>
           <th>Category Name</th>
           <th>GST Rate</th>
+          <th>Action</th>
+          <th>Update GST</th>
         </tr>
       </thead>
       <tbody>
@@ -14,6 +16,12 @@ const CategoryList = ({ categories }) => {
           <tr key={index}>
             <td>{category.name}</td>
             <td>{category.gst}</td>
+            <td>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </td>
+            <td>
+              <button onClick={() => handleGSTUpdate(index)}>Update GST</button>
+            </td>
           </tr>
         ))}
       </tbody>
