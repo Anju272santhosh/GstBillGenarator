d8146efe3bb31ab0e938bf74039afc138f2eaaad
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-05 23:29:14 +0530

    fix(ProductList): Delete button added

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products, handleDelete }) => {
   return (
     <table>
       <thead>
@@ -8,7 +8,9 @@ const ProductList = ({ products }) => {
           <th>Product Name</th>
           <th>Category</th>
           <th>Price</th>
+          <th>Quantity</th>
           {/* <th>Tax</th> */}
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
@@ -17,7 +19,11 @@ const ProductList = ({ products }) => {
             <td>{product.name}</td>
             <td>{product.category}</td>
             <td>{product.price}</td>
+            <td>{product.quantity || 1}</td>
             {/* <td>{product.tax}</td> */}
+            <td>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </td>
           </tr>
         ))}
       </tbody>
