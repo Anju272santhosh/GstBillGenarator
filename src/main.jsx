d7e11b2f0b0b4db6f176fff234c656a04e43cfa7
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-06 16:19:19 +0530

    fix(main): Final update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Invoice from "./Bill.jsx";
+// import Invoice from "./Bill.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
