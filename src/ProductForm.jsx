5cd3c3629c8ac99882a730bd464d1766a189d743
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-06 16:18:39 +0530

    fix(ProductForm): Final update

--- src/ProductForm.jsx
+++ src/ProductForm.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 
 const ProductForm = ({ categories, addProduct }) => {
+  // State for managing form inputs
   const [productName, setProductName] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
   const [productQuantity, setProductQuantity] = useState('');
   const [productPrice, setProductPrice] = useState('');
 
+  // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Calculate tax and add product with details to the list
     addProduct({
       name: productName,
       category: selectedCategory,
@@ -15,15 +18,16 @@ const ProductForm = ({ categories, addProduct }) => {
       price: parseFloat(productPrice),
       tax: calculateTax(selectedCategory, parseFloat(productPrice)),
     });
+    // Reset form input fields after adding the product
     setProductName('');
     setSelectedCategory('');
     setProductQuantity('');
     setProductPrice('');
   };
 
+  // Function to calculate tax based on category and price
   const calculateTax = (category, price) => {
-    // Your tax calculation logic here
-    // Example logic:
+    // Tax calculation logic based on category
     if (category === 'Electronics') {
       return price * 0.2; // Assuming 20% tax for Electronics
     } else if (category === 'Food') {
@@ -35,23 +39,27 @@ const ProductForm = ({ categories, addProduct }) => {
 
   return (
     <form onSubmit={handleSubmit}>
+      {/* Input fields for product details */}
       <input
         type="text"
         value={productName}
         onChange={(e) => setProductName(e.target.value)}
         placeholder="Product Name"
       />
+      {/* Dropdown to select category */}
       <select
         value={selectedCategory}
         onChange={(e) => setSelectedCategory(e.target.value)}
       >
         <option value="">Select Category</option>
+        {/* Displaying available categories */}
         {categories.map((category, index) => (
           <option key={index} value={category.name}>
             {category.name}
           </option>
         ))}
       </select>
+      {/* Input fields for quantity and price */}
       <input
         type="number"
         value={productQuantity}
@@ -64,6 +72,7 @@ const ProductForm = ({ categories, addProduct }) => {
         onChange={(e) => setProductPrice(e.target.value)}
         placeholder="Product Price"
       />
+      {/* Button to submit the form */}
       <button type="submit">Add Product</button>
     </form>
   );
