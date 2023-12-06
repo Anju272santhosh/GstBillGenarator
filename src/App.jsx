38b4577c0f1eae5e69902e5133fc2abc42f5660d
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-06 12:20:06 +0530

    fix(App): Added Update Gst

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import CategoryForm from './CategoryForm';
 import ProductForm from './ProductForm';
 import CategoryList from './CategoryList';
 import ProductList from './ProductList';
 import GSTForm from './GstForm';
 import Bill from './Bill';
+import './style.css';
 
 const App = () => {
   const [categories, setCategories] = useState(() => {
@@ -37,9 +38,31 @@ const App = () => {
     localStorage.setItem('products', JSON.stringify(products));
   }, [products]);
 
+  const handleDelete = (index) => {
+    const updatedCategories = [...categories];
+    updatedCategories.splice(index, 1);
+    setCategories(updatedCategories);
+  };
+
+  const handleDeleteProduct = (index) => {
+    const updatedProducts = [...products];
+    updatedProducts.splice(index, 1);
+    setProducts(updatedProducts);
+  };
+
+  const handleGSTUpdate = (index, newGSTValue) => {
+    const updatedCategories = [...categories];
+    updatedCategories[index].gst = newGSTValue; // Update the GST rate for the specific category
+    setCategories(updatedCategories);
+  };
+  
+
+
+
   return (
     <Router>
-      <div>
+    <div className="container">
+      <div className="card">
         <h1>GST System</h1>
 
         {/* Navigation bar */}
@@ -68,6 +91,15 @@ const App = () => {
 
         {/* Routes */}
         <Routes>
+        <Route path="/" element={<Home />} /> {/* Render the Home component at the root path */}
+            <Route
+              path="/add-category"
+              element={<CategoryForm addCategory={addCategory} />}
+            />
+
+
+
+
           <Route
             path="/add-category"
             element={<CategoryForm addCategory={addCategory} />}
@@ -81,17 +113,38 @@ const App = () => {
             element={<Bill products={products} />}
           />
           <Route
-            path="/category-list"
-            element={<CategoryList categories={categories} />}
+          path="/category-list"
+        element={
+        <CategoryList
+        categories={categories}
+        handleDelete={handleDelete}
+        handleGSTUpdate={handleGSTUpdate}/>}
           />
+
           <Route
             path="/product-list"
-            element={<ProductList products={products} />}
-          />
+            element={
+              <ProductList
+                products={products}
+                handleDelete={handleDeleteProduct}/>}
+              />
+
         </Routes>
       </div>
+    </div>
     </Router>
   );
 };
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to GST System</h2>
+      <p>This is the home page content.
+      Hi h</p>
+    </div>
+  );
+};
+
+
 
 export default App;
