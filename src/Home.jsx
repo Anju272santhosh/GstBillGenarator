6ac7496704d5ddebaaddf5605397ab3b4bffefb6
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-06 16:19:01 +0530

    fix(Home): Final update

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import CategoryForm from './CategoryForm';
 import ProductForm from './ProductForm';
 import Bill from './Bill';
-import './styles.css'; // Import your CSS file for styling
+import './styles.css'; 
 
 const Home = () => {
   return (
     <div className="container">
+      {/* Set up the router for navigation */}
       <Router>
+        {/* Navigation bar */}
         <div className="navbar">
           <ul>
+            {/* Links to different sections of the GST System */}
             <li>
               <Link to="/add-category">Add Category</Link>
             </li>
@@ -23,15 +26,16 @@ const Home = () => {
           </ul>
         </div>
 
+        {/* Main content area */}
         <div className="content">
           <h2>Welcome to the GST System!</h2>
           <p>
             This is the homepage of our GST System. You can navigate using the links above.
           </p>
-
-          {/* Additional content for the home page */}
+          {/* This area can display additional content or instructions */}
         </div>
 
+        {/* Define routes for each section */}
         <Route path="/add-category" component={CategoryForm} />
         <Route path="/add-product" component={ProductForm} />
         <Route path="/generate-bill" component={Bill} />
