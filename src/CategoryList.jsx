24b3fe25b44058ca42e61522402ed5f2ca171125
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-06 12:22:15 +0530

    fix(Bill): fixed tax calculation

--- src/CategoryList.jsx
+++ src/CategoryList.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CategoryList = ({ categories, handleDelete, handleGSTUpdate }) => {
+  const [newGSTValues, setNewGSTValues] = useState(Array(categories.length).fill(''));
+
+  const updateNewGSTValue = (index, value) => {
+    const updatedValues = [...newGSTValues];
+    updatedValues[index] = value;
+    setNewGSTValues(updatedValues);
+  };
+
+  const handleGSTUpdateClick = (index) => {
+    handleGSTUpdate(index, newGSTValues[index]);
+    setNewGSTValues(Array(categories.length).fill(''));
+  };
+
   return (
     <table>
       <thead>
@@ -20,7 +33,12 @@ const CategoryList = ({ categories, handleDelete, handleGSTUpdate }) => {
               <button onClick={() => handleDelete(index)}>Delete</button>
             </td>
             <td>
-              <button onClick={() => handleGSTUpdate(index)}>Update GST</button>
+              <input
+                type="text"
+                value={newGSTValues[index]}
+                onChange={(e) => updateNewGSTValue(index, e.target.value)}
+              />
+              <button onClick={() => handleGSTUpdateClick(index)}>Update</button>
             </td>
           </tr>
         ))}
