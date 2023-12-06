fb9a44d30c88c5622215868390b3079443282f86
Anju272santhosh <anjusanthosh207@gmail.com>
2023-12-06 16:15:57 +0530

    fix(Bill): Print module added

--- src/Bill.jsx
+++ src/Bill.jsx
@@ -1,53 +1,88 @@
 import React from 'react';
 
 const Bill = ({ products }) => {
-  // Check if products are available to generate the bill
+  // Function to handle printing the bill details
+  const handlePrint = () => {
+    // Get the bill details section to print
+    const billDetails = document.querySelector('.bill');
+    
+    // Create a new window for printing
+    const printWindow = window.open('', '_blank');
+
+    if (printWindow) {
+      // Write content to the new window
+      printWindow.document.write('<html><head><title>Bill Details</title>');
+      printWindow.document.write('</head><body>');
+      printWindow.document.write(billDetails.innerHTML); // Write bill details content to the new window
+      printWindow.document.write('</body></html>');
+
+      printWindow.document.close(); // Close the document to render content
+      printWindow.print(); // Print the new window content
+    } else {
+      console.log('Could not open print window.'); // Handle if the window failed to open
+    }
+  };
+
+  let content;
+
   if (products.length === 0) {
-    return (
-      <div>
+    // Display message if no items are added
+    content = (
+      <div className='Nobill'>
         <h2>No Items Added</h2>
         <p>Please select a category and product to generate a bill.</p>
       </div>
     );
+  } else {
+    // Calculate subtotal, total tax, and total bill amount
+    const subtotal = products.reduce((total, product) => total + product.price, 0);
+    const totalTax = products.reduce((total, product) => total + product.tax, 0);
+    const totalBill = subtotal + totalTax;
+
+    content = (
+      <div className='bill'>
+        {/* Bill details */}
+        <h2>Bill Details</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Tax</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Display each product */}
+            {products.map((product, index) => (
+              <tr key={index}>
+                <td>{product.name}</td>
+                <td>{product.category}</td>
+                <td>${product.price}</td>
+                <td>${product.tax}</td>
+                <td>${product.price + product.tax}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {/* Bill summary */}
+        <div className="bill-summary">
+          <p>Subtotal: ${subtotal}</p>
+          <p>Total Tax: ${totalTax}</p>
+          <p>Total Bill Amount: ${totalBill}</p>
+        </div>
+
+        {/* Print button */}
+        <button className="print-button" onClick={handlePrint}>
+          Print Bill
+        </button>
+      </div>
+    );
   }
 
-  const subtotal = products.reduce((total, product) => total + product.price, 0);
-  const totalTax = products.reduce((total, product) => total + product.tax, 0);
-  const totalBill = subtotal + totalTax;
-
-  return (
-    <div>
-      <h2>Bill Details</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Product Name</th>
-            <th>Category</th>
-            <th>Price</th>
-            <th>Tax</th>
-            <th>Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product, index) => (
-            <tr key={index}>
-              <td>{product.name}</td>
-              <td>{product.category}</td>
-              <td>${product.price}</td>
-              <td>${product.tax}</td>
-              <td>${product.price + product.tax}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <div className="bill-summary">
-        <p>Subtotal: ${subtotal}</p>
-        <p>Total Tax: ${totalTax}</p>
-        <p>Total Bill Amount: ${totalBill}</p>
-      </div>
-    </div>
-  );
+  return content;
 };
 
 export default Bill;
